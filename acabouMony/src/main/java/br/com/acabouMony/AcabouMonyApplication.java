961bf8d1ccd248e7f54787a6914d4f60954ba63e
maikonprojects <162837965+maikonprojects@users.noreply.github.com>
2025-05-20 10:24:09 -0300

    subindo estrutura do projeto

---
+++ acabouMony/src/main/java/br/com/acabouMony/AcabouMonyApplication.java
@@ -0,0 +1,13 @@
+package br.com.acabouMony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcabouMonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcabouMonyApplication.class, args);
+	}
+
+}
---
+++ acabouMony/src/test/java/br/com/acabouMony/AcabouMonyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acabouMony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcabouMonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
