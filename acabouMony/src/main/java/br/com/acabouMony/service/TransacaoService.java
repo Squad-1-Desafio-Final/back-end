e819b5428f9a39fc99d0b3df68c72270c9943396
maikonprojects <maikon.projetos@gmail.com>
2025-05-20 13:22:10 -0300

    UP: Ajustando anotação e service

--- acabouMony/src/main/java/br/com/acabouMony/service/TransacaoService.java
+++ acabouMony/src/main/java/br/com/acabouMony/service/TransacaoService.java
@@ -1,13 +1,23 @@
 package br.com.acabouMony.service;
 
 import br.com.acabouMony.dto.CadastroTransacaoDto;
+import br.com.acabouMony.entity.Cartao;
+import br.com.acabouMony.entity.Produto;
+import br.com.acabouMony.entity.Transacao;
 import br.com.acabouMony.entity.Usuario;
+import br.com.acabouMony.exception.CartaoNaoEncontrado;
 import br.com.acabouMony.exception.UsuarioNaoEncontradoException;
+import br.com.acabouMony.mapper.TransacaoCadastroMapper;
+import br.com.acabouMony.repository.CartaoRepository;
 import br.com.acabouMony.repository.ProdutoRepository;
 import br.com.acabouMony.repository.TransacaoRepository;
 import br.com.acabouMony.repository.UsuarioRepository;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TransacaoService {
@@ -17,12 +27,30 @@ public class TransacaoService {
     @Autowired
     UsuarioRepository usuarioRepository;
     @Autowired
-    ProdutoRepository produtoRepository;
+    CartaoRepository cartaoRepository;
+
+    @Autowired
+    TransacaoCadastroMapper transacaoCadastroMapper;
 
     public CadastroTransacaoDto criar (CadastroTransacaoDto dados){
 
         Usuario usuario = usuarioRepository.findById(dados.usuario().getId())
                 .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado"));
+
+        Cartao cartao = cartaoRepository.findById(dados.cartao().getId())
+                .orElseThrow(() -> new CartaoNaoEncontrado("Cartão não encontrado"));
+
+        Transacao transacao = transacaoCadastroMapper.toEntity(dados);
+
+        Transacao transacaoSalva = repository.save(transacao);
+
+        return transacaoCadastroMapper.toTransacaoDto(transacaoSalva);
+
+    }
+    public List<Transacao> listar (){
+
+      return repository.findAll();
+
     }
 
 }
