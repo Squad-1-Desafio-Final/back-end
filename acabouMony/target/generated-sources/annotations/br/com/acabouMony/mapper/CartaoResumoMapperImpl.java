5638731d80446378080662e633a23dad19d85d6e
maikonprojects <maikon.projetos@gmail.com>
2025-05-22 11:47:40 -0300

    Teste mapping mapper

--- acabouMony/target/generated-sources/annotations/br/com/acabouMony/mapper/CartaoResumoMapperImpl.java
+++ acabouMony/target/generated-sources/annotations/br/com/acabouMony/mapper/CartaoResumoMapperImpl.java
@@ -2,14 +2,13 @@ package br.com.acabouMony.mapper;
 
 import br.com.acabouMony.dto.CartaoResumoDto;
 import br.com.acabouMony.entity.Cartao;
-import br.com.acabouMony.tipos.TipoPagamento;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-22T09:43:36-0300",
-    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.15 (Amazon.com Inc.)"
+    date = "2025-05-22T11:40:07-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Amazon.com Inc.)"
 )
 @Component
 public class CartaoResumoMapperImpl implements CartaoResumoMapper {
@@ -22,11 +21,6 @@ public class CartaoResumoMapperImpl implements CartaoResumoMapper {
 
         Cartao cartao = new Cartao();
 
-        cartao.setNumero( (long) dto.numero() );
-        if ( dto.tipo() != null ) {
-            cartao.setTipo( Enum.valueOf( TipoPagamento.class, dto.tipo() ) );
-        }
-
         return cartao;
     }
 
@@ -38,13 +32,6 @@ public class CartaoResumoMapperImpl implements CartaoResumoMapper {
 
         int numero = 0;
         String tipo = null;
-
-        if ( entity.getNumero() != null ) {
-            numero = entity.getNumero().intValue();
-        }
-        if ( entity.getTipo() != null ) {
-            tipo = entity.getTipo().name();
-        }
 
         CartaoResumoDto cartaoResumoDto = new CartaoResumoDto( numero, tipo );
 
--- acabouMony/target/generated-sources/annotations/br/com/acabouMony/mapper/PedidoListarMapperStructImpl.java
+++ acabouMony/target/generated-sources/annotations/br/com/acabouMony/mapper/PedidoListarMapperStructImpl.java
@@ -4,7 +4,12 @@ import br.com.acabouMony.dto.ListagemPedidoDto;
 import br.com.acabouMony.dto.UsuarioResumoDto;
 import br.com.acabouMony.entity.Pedido;
 import br.com.acabouMony.entity.Produto;
+import br.com.acabouMony.entity.Usuario;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import javax.annotation.processing.Generated;
@@ -12,8 +17,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-22T10:28:22-0300",
-    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.15 (Amazon.com Inc.)"
+    date = "2025-05-22T11:44:53-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Amazon.com Inc.)"
 )
 @Component
 public class PedidoListarMapperStructImpl implements PedidoListarMapperStruct {
@@ -25,6 +30,18 @@ public class PedidoListarMapperStructImpl implements PedidoListarMapperStruct {
         }
 
         Pedido pedido = new Pedido();
+
+        pedido.setId( dto.id() );
+        pedido.setUsuario( usuarioResumoDtoToUsuario( dto.usuario() ) );
+        List<Produto> list = dto.produtos();
+        if ( list != null ) {
+            pedido.setProdutos( new ArrayList<Produto>( list ) );
+        }
+        pedido.setPrecoTotal( dto.precoTotal() );
+        if ( dto.date() != null ) {
+            pedido.setDate( Date.from( dto.date().toInstant( ZoneOffset.UTC ) ) );
+        }
+        pedido.setCarrinho( dto.carrinho() );
 
         return pedido;
     }
@@ -41,6 +58,22 @@ public class PedidoListarMapperStructImpl implements PedidoListarMapperStruct {
         double precoTotal = 0.0d;
         LocalDateTime date = null;
         boolean carrinho = false;
+
+        id = entity.getId();
+        usuario = usuarioToUsuarioResumoDto( entity.getUsuario() );
+        List<Produto> list = entity.getProdutos();
+        if ( list != null ) {
+            produtos = new ArrayList<Produto>( list );
+        }
+        if ( entity.getPrecoTotal() != null ) {
+            precoTotal = entity.getPrecoTotal();
+        }
+        if ( entity.getDate() != null ) {
+            date = LocalDateTime.ofInstant( entity.getDate().toInstant(), ZoneId.of( "UTC" ) );
+        }
+        if ( entity.getCarrinho() != null ) {
+            carrinho = entity.getCarrinho();
+        }
 
         ListagemPedidoDto listagemPedidoDto = new ListagemPedidoDto( id, usuario, produtos, precoTotal, date, carrinho );
 
@@ -60,8 +93,56 @@ public class PedidoListarMapperStructImpl implements PedidoListarMapperStruct {
         LocalDateTime date = null;
         boolean carrinho = false;
 
+        id = entity.getId();
+        usuario = usuarioToUsuarioResumoDto( entity.getUsuario() );
+        List<Produto> list = entity.getProdutos();
+        if ( list != null ) {
+            produtos = new ArrayList<Produto>( list );
+        }
+        if ( entity.getPrecoTotal() != null ) {
+            precoTotal = entity.getPrecoTotal();
+        }
+        if ( entity.getDate() != null ) {
+            date = LocalDateTime.ofInstant( entity.getDate().toInstant(), ZoneId.of( "UTC" ) );
+        }
+        if ( entity.getCarrinho() != null ) {
+            carrinho = entity.getCarrinho();
+        }
+
         ListagemPedidoDto listagemPedidoDto = new ListagemPedidoDto( id, usuario, produtos, precoTotal, date, carrinho );
 
         return listagemPedidoDto;
+    }
+
+    protected Usuario usuarioResumoDtoToUsuario(UsuarioResumoDto usuarioResumoDto) {
+        if ( usuarioResumoDto == null ) {
+            return null;
+        }
+
+        Usuario usuario = new Usuario();
+
+        usuario.setId( usuarioResumoDto.id() );
+        usuario.setNome( usuarioResumoDto.nome() );
+        usuario.setEmail( usuarioResumoDto.email() );
+
+        return usuario;
+    }
+
+    protected UsuarioResumoDto usuarioToUsuarioResumoDto(Usuario usuario) {
+        if ( usuario == null ) {
+            return null;
+        }
+
+        UUID id = null;
+        String nome = null;
+        String email = null;
+
+        id = usuario.getId();
+        nome = usuario.getNome();
+        email = usuario.getEmail();
+
+        UsuarioResumoDto usuarioResumoDto = new UsuarioResumoDto( id, nome, email );
+
+        return usuarioResumoDto;
     }
 }
